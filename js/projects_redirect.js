bb4c54d60c61c476d8051a002a25f8bb9c8d95f3
dhruvk19 <121083440+dhruvk19@users.noreply.github.com>
2023-11-29 13:41:19 -0500

    Add files via upload

---
+++ js/projects_redirect.js
@@ -0,0 +1,9 @@
+function redirectToSIProject() {
+    window.location.href = 'https://github.com/dhruvk19';
+}
+// function redirectToGitHub() {
+//     window.location.href = 'https://github.com/dhruvk19';
+// }
+function redirectToOR() {
+    window.location.href = 'https://openreview.net/profile?id=~Dhruv_Kapur1';
+}
---
+++ js/redirect_icon.js
@@ -0,0 +1,10 @@
+function redirectToLinkedIn() {
+    window.location.href = 'https://www.linkedin.com/in/dhruvk19/';
+}
+function redirectToGitHub() {
+    window.location.href = 'https://github.com/dhruvk19';
+}
+
+function redirectToOR() {
+    window.location.href = 'https://openreview.net/profile?id=~Dhruv_Kapur1';
+}
